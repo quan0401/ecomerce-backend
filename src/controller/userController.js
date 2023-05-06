46bb825d216128608720e9ec5b3a8c4900dae90e
quan0401 <dongminhquan2004@gmail.com>
2023-05-06 15:53:19 +0700

    updateUserProfile, getUserProfile, writeReview for product api

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,9 @@
 import User from "../models/UserModel";
+import Review from "../models/ReviewModel";
+import mongoose from "mongoose";
 import generateAuthToken from "../utils/generateAuthToken";
 import { comparePassword, hassPassword } from "../utils/password";
+import Product from "../models/ProductModel";
 require("dotenv").config();
 
 export const getAll = async (req, res, next) => {
@@ -70,7 +73,7 @@ export const login = async (req, res, next) => {
       secure: process.env.NODE_ENV === "production",
     };
 
-    if (doNotLogout) cookieParams["maxAge"] = 300 * 1000;
+    if (doNotLogout) cookieParams["maxAge"] = 300 * 1000 * 6;
 
     res
       .cookie(
@@ -95,6 +98,92 @@ export const login = async (req, res, next) => {
           isAdmin: user.isAdmin,
         },
       });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).orFail();
+    const check = comparePassword(req.body.oldPassword, user.password);
+    if (!check) res.status(401).send("Wrong password");
+
+    user.firstName = req.body.firstName || user.firstName;
+    user.lastName = req.body.lastName || user.lastName;
+    user.email = req.body.email || user.email;
+    if (req.body.oldPassword !== req.body.password)
+      user.password = hassPassword(req.body.password);
+    user.phoneNumber = req.body.phoneNumber;
+    user.country = req.body.country;
+    user.city = req.body.city;
+    user.zipCode = req.body.zipCode;
+
+    const result = await user.save();
+
+    res.status(201).json({
+      success: "Update success",
+      userUpdated: {
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .select("-password")
+      .orFail();
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const writeReview = async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    const { rating, comment } = req.body;
+
+    // Create reviewId manually since it also need to save for product
+    const reviewId = new mongoose.Types.ObjectId();
+    const result = await Review.create({
+      _id: reviewId,
+      rating: Number(rating),
+      comment,
+      user: {
+        _id: req.user._id,
+        name: req.user.firstName + " " + req.user.lastName,
+      },
+    });
+
+    const product = await Product.findById(productId)
+      .populate("reviews")
+      .orFail();
+
+    // Calculate total rating
+    const totalRating =
+      product.reviews.reduce(
+        (acc, currentValue) => Number(currentValue.rating) + acc,
+        0
+      ) + Number(rating);
+
+    product.reviews.push(reviewId);
+
+    product.reviewsNumber = product.reviews.length;
+    product.rating = totalRating / product.reviewsNumber;
+
+    const save = await product.save();
+
+    res.status(200).send({ product: save });
   } catch (error) {
     next(error);
   }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,27 @@
 import express from "express";
-import { getAll, registerUser, login } from "../controller/userController";
+import {
+  getAll,
+  registerUser,
+  login,
+  updateUserProfile,
+  getUserProfile,
+  writeReview,
+} from "../controller/userController";
+import { verifyAdmin, verifyIsLoggedIn } from "../middleware/verifyAuthToken";
 
 const userRoutes = express.Router();
 
-userRoutes.get("/", getAll);
 userRoutes.post("/register", registerUser);
 userRoutes.post("/login", login);
+
+// Middleware logged in
+userRoutes.use(verifyIsLoggedIn);
+userRoutes.get("/profile/:id", getUserProfile);
+userRoutes.put("/profile", updateUserProfile);
+userRoutes.post("/review/:productId", writeReview);
+
+// Admin routes
+userRoutes.use(verifyAdmin);
+userRoutes.get("/", getAll);
 
 export default userRoutes;
